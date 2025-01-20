36d29176054fca077d3d68f3856d8bf419f924c1
Alanhsiu <alan901101@gmail.com>
2025-01-20 16:25:23 +0800

    update .gitgonore and run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-# mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release ../src && make
-mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug ../src && make
+mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release ../src && make
+# mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug ../src && make
 
 current_path="/home/b09901066/ISPD-NTUEE/NTUGR_v2"
 docker_data_path="/docker_data/b09901066"
@@ -8,11 +8,11 @@ input_path=$current_path"/input"
 output_PR_path=$current_path"/output"
 output_log_path=$current_path"/log"
 
-echo "Running example"
-./route -def example.def -v example.v.gz -sdc example.sdc -cap $input_path/example.cap -net $input_path/example.net -output $output_PR_path/example.route > $output_log_path/example.log
+# echo "Running example"
+# ./route -def example.def -v example.v.gz -sdc example.sdc -cap $input_path/example.cap -net $input_path/example.net -output $output_PR_path/example.route > $output_log_path/example.log
 
-# echo "Running ariane"
-# ./route -cap $input_path/ariane.cap -net $input_path/ariane.net -output $output_PR_path/ariane.route > $output_log_path/ariane.log
+echo "Running ariane"
+./route -cap $input_path/ariane.cap -net $input_path/ariane.net -output $output_PR_path/ariane.route > $output_log_path/ariane.log
 
 # cd ..
 # cd evaluation/
